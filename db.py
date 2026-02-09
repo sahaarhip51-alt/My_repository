d9e0492da2c6c1d9f51fd47c05b82f45c94b50da
Архипов Александр <sahaarhip51@gmail.com>
2026-02-09 19:02:00 +0300

    commit

--- db.py
+++ db.py
@@ -1,2 +1,12 @@
-print("Here will be my db")
-print("Let,s go!")
+#print("Here will be my db")
+#print("Let,s go!")
+import mariadb
+conn = mariadb.connect(
+    host="84.38.180.130",
+    port=3306,
+    user="sahaarhip51",
+    password="Sahaissaha",
+    database="sahaarhip51_db",
+    autocommit=False   # важно для UPDATE / DELETE
+)
+cursor = conn.cursor()
